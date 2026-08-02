e198b66b88e3f6bc02091c6648f64c1c531127c2
Bhanuprakash Nallagorla <bhanuprakash.60h@gmail.com>
2026-08-02 20:01:50 +0530

    Time: 0 ms (100%), Space: 19.4 MB (2.68%) - LeetHub

--- 3688-bitwise-or-of-even-numbers-in-an-array/3688-bitwise-or-of-even-numbers-in-an-array.py
+++ 3688-bitwise-or-of-even-numbers-in-an-array/3688-bitwise-or-of-even-numbers-in-an-array.py
@@ -1,6 +1,7 @@
 class Solution:
     def evenNumberBitwiseORs(self, nums: List[int]) -> int:
         bor=0
+        nums=set(nums)
         for i in nums:
             if i&1==0: bor|=i
         return bor
