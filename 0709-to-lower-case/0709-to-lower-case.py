2f964bb940f4cfdbee43018532cb680a0d45a066
Bhanuprakash Nallagorla <bhanuprakash.60h@gmail.com>
2026-08-04 12:39:29 +0530

    Time: 0 ms (100%), Space: 19.1 MB (97.29%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,4 +1,10 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+        # return s.lower()
+        l=""
+        for i in s:
+            if i.isupper():
+                l+=chr(ord(i)+32)
+            else:l+=i
+        return l
         
